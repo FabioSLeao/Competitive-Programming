d05e0019741f0c28d4135dd6ed34646cca2e2775
Kali <fabioleao48@gmail.com>
2021-02-06 20:31:59 -0500

    logic fixed

--- 10550_Combination-Lock.cpp
+++ 10550_Combination-Lock.cpp
@@ -11,13 +11,13 @@ int main(){
 
      int angle{1080};
 
-     angle += comb[0]*9;
-     angle += abs(comb[1]-40)*9;
-     angle += (40+comb[2]-comb[1])*9;
-     angle += abs(comb[3]-(40+comb[2]))*9;
+     angle += ((comb[0] - comb[1]+40)%40)*9; //clockwise
+     angle += ((comb[2] - comb[1]+40)%40)*9; //counter-clockwise
+     angle += ((comb[2] - comb[3] + 40)%40)*9; //clockwise
+
      cout << angle << endl;
  }
-    
+
 return 0;
 
 }
